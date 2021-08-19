34f787dc7828e393a36c84e8a4189897a820a44b
Wiktor Kopec <wiktork@microsoft.com>
2021-08-19 10:10:26 -0700

    Aspnet trigger

---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Triggers/AspnetTriggerProto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe.Triggers
+{
+    internal class AspnetTriggerSettings { }
+
+    internal class AspnetTriggerProtoFactory : ITraceEventTriggerFactory<AspnetTriggerSettings>
+    {
+        public ITraceEventTrigger Create(AspnetTriggerSettings settings)
+        {
+            return new AspnetTriggerProto();
+        }
+    }
+
+    internal class AspnetTriggerProto : ITraceEventTrigger
+    {
+        private readonly Dictionary<string, IEnumerable<string>> _providerMap;
+
+        public AspnetTriggerProto()
+        {
+            _providerMap = new()
+            {
+                { "Microsoft-Diagnostics-DiagnosticSource", null }
+            };
+        }
+
+        public IDictionary<string, IEnumerable<string>> GetProviderEventMap()
+        {
+            return _providerMap;
+        }
+
+        public bool HasSatisfiedCondition(TraceEvent traceEvent)
+        {
+            return true;
+        }
+    }
+}
