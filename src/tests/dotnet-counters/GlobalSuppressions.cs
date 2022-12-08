bdfb4ad3888d295a8432e06070a28d1bc7c10032
wiktork <wiktork@microsoft.com>
2022-12-08 09:34:53 -0800

    [Temp] Global suppressions for faulty analysis errors

---
+++ src/tests/dotnet-counters/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'counterType'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.counterType")]
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'name'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.name")]
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'provider'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.provider")]
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'tags'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.tags")]
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'timestamp'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.timestamp")]
+[assembly: SuppressMessage("Build", "CA1507:Use nameof in place of string literal 'value'", Justification = "<Pending>", Scope = "member", Target = "~P:DotnetCounters.UnitTests.JSONCounterPayload.value")]
---
+++ src/tests/dotnet-trace/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Build", "CA1825:Avoid unnecessary zero-length array allocations.  Use Array.Empty<object>() instead.", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.Diagnostics.Tools.Trace.ChildProcessTests.VerifyExitCode(Microsoft.Diagnostics.TestHelpers.TestConfiguration)")]
+[assembly: SuppressMessage("Build", "CA1825:Avoid unnecessary zero-length array allocations.  Use Array.Empty<object>() instead.", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.Diagnostics.Tools.Trace.ChildProcessTests.VerifyHideIO(Microsoft.Diagnostics.TestHelpers.TestConfiguration)")]
+[assembly: SuppressMessage("Build", "CA1825:Avoid unnecessary zero-length array allocations.  Use Array.Empty<object>() instead.", Justification = "<Pending>", Scope = "member", Target = "~M:Microsoft.Diagnostics.Tools.Trace.ChildProcessTests.VerifyShowIO(Microsoft.Diagnostics.TestHelpers.TestConfiguration)")]
